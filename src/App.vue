139b16a158cd649fc478f42858e170d1e82d0aaa
mferreiradb <mauhmg@gmail.com>
2023-06-24 15:56:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <div id="layout">
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink> | 
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+      <RouterView />
+    </div>
+  </header>
+</template>
+
+<style>
+#layout {
+  font-family: Acvenir, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -webkit-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+h2 {
+  font-size: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue';
+import { ref } from 'vue';
+
+const events = ref([
+{
+  id: 1,
+  category: 'Animal',
+  title: 'Cat adoption day',
+  description: 'Find you a new feline friend at this event.',
+  location: 'Meow town',
+  date: 'January 28, 2023',
+  time: '22:00',
+  petAllowed: true,
+  organizer: 'Kat Laydee '
+},
+{
+  id: 2,
+  category: 'Animal',
+  title: 'Monkey adoption day',
+  description: 'Find you a new monkey friend at this event.',
+  location: 'HUHAHAHA town',
+  date: 'January 29, 2023',
+  time: '22:00',
+  petAllowed: true,
+  organizer: 'Monk Laydee '
+},
+{
+  id: 3,
+  category: 'Animal',
+  title: 'Dog adoption day',
+  description: 'Find you a new dog friend at this event.',
+  location: 'Doo town',
+  date: 'January 30, 2023',
+  time: '22:00',
+  petAllowed: true,
+  organizer: 'Scoob Laydee '
+}
+])
+
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<style scoped>
+  .events {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
